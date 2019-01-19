94c0d55806c30e204991632f4d07d0a2d8cdba41
DatDz <Keepyoursmile.512@gmail.com>
2019-01-19 11:56:03 +0700

    no message

--- Assets/_Scripts/Heroes/Hero.cs
+++ Assets/_Scripts/Heroes/Hero.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public abstract class Hero : MonoBehaviour
 {
     public InfoHero infoHero = new InfoHero();
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -17,9 +18,13 @@ public abstract class Hero : MonoBehaviour
 
     public abstract void SetInfoHero();
 
+    public abstract void CheckEnemy();
+
     public abstract void MoveToPosition(Vector2 _toPos);
 
     public abstract void Attack();
+
+    public abstract void BeingAttacked(int _dame);
 
     public abstract void Die();
 }
