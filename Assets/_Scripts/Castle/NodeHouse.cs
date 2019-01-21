bad92f86747c4a364a1e0dedaeb7ddcb9918c099
DatDz <Keepyoursmile.512@gmail.com>
2019-01-21 17:56:25 +0700

    them panel

---
+++ Assets/_Scripts/Castle/NodeHouse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodeHouse : MonoBehaviour {
+    public int id;
+    public int level;
+    public bool isBuild;
+    public int countHero;
+
+    public Button buttonUpgrade;
+    public Button buttonRelease;
+    public Text txtCountHero;
+    public Image imgHouse;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isBuild && txtCountHero.gameObject.activeSelf)
+        {
+            txtCountHero.text = countHero.ToString();
+        }
+	}
+
+    public void Btn_OnClick()
+    {
+        if (isBuild)
+        {
+            buttonUpgrade.gameObject.SetActive(true);
+            buttonRelease.gameObject.SetActive(true);
+        }
+        else
+        {
+            UIManager.Instance.ShowPanelBuild();
+        }
+    }
+}
