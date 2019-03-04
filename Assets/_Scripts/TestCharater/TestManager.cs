65fdb27942801907f0c3eae7aa982f8ed19d97f8
DESKTOP-ARRSUT8\TienLoc <nguyenloc271096@gmail.com>
2019-03-04 17:36:23 +0700

    Ins Map

--- Assets/_Scripts/TestCharater/TestManager.cs
+++ Assets/_Scripts/TestCharater/TestManager.cs
@@ -1,11 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TestManager : MonoBehaviour
 {
+    public GameObject prefabsBoxMap;
+    public int row;
+    public int col;
+    public int weight;
+    public Transform boxManager;
 
-   
+    private Vector2[] PosGolds = new Vector2[] { new Vector2(3, 3), new Vector2(0, 0), new Vector2(0, 1), new Vector2(0, 2), new Vector2(1, 0), new Vector2(1, 2), new Vector2(2, 2), new Vector2(2, 3), new Vector2(2, 4), new Vector2(2, 5), new Vector2(2, 6), new Vector2(3, 6), new Vector2(4, 1), new Vector2(4, 2), new Vector2(4, 3), new Vector2(4, 4), new Vector2(4, 5), new Vector2(4, 6), new Vector2(5, 3), new Vector2(5, 4) };
+
+    public void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                Instantiate(prefabsBoxMap, new Vector3(i * weight, -j * weight), Quaternion.identity, boxManager);
+                if(Array.Find(PosGolds, sound => sound.x == col && sound.y == row) != null)
+                {
+
+                }
+            }
+        }
+    }
 
 }
