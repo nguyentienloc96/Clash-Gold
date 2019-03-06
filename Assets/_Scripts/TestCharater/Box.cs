5a7f57698065dec8619259f8e72969257f390498
DESKTOP-ULE6MGM\nguye <nguyenloc271096@gmail.com>
2019-03-06 09:58:17 +0700

    Code PathFinding

---
+++ Assets/_Scripts/TestCharater/Box.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+    public int col;
+    public int row;
+    public bool isLock;
+    public bool isLeft;
+    public bool isRight;
+    public bool isTop;
+    public bool isBottom;
+}
